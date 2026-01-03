1d209ecca0693d95e90cbf9eae32d46b2d0320b7
xiaotian-scienceos <xiaotian@scienceos.tech>
2026-01-03 21:36:55 +0800

    vite doc

---
+++ demo/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      less: {},
+    },
+  },
+  build: {
+    outDir: "dist",
+    minify: "esbuild",
+  },
+  esbuild: {},
+});
